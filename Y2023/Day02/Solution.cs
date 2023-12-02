0ab4c33fdcf7edfbf8c2c41c4e11843a2a828dd0
Nathan Ortbals <nathan.ortbals@gmail.com>
2023-12-02 09:41:26 -0600

    Fixed top level namespace

--- Y2023/Day02/Solution.cs
+++ Y2023/Day02/Solution.cs
@@ -1,95 +1,94 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace AdventOfCode.Y2023.Day02
+namespace AdventOfCode.Y2023.Day02;
+
+internal class Solution : AdventOfCode.Solution
 {
-    internal class Solution : AdventOfCode.Solution
+    public override int Year => 2023;
+
+    public override int Day => 02;
+
+    public override string Name => "Cube Conundrum";
+
+    private record Set(int RedCubes, int GreenCubes, int BlueCubes);
+
+    private record Game(int Id, Set[] Sets);
+
+    protected override int SolvePart1(string input)
     {
-        public override int Year => 2023;
+        var games = ParseGames(input);
 
-        public override int Day => 02;
+        var maxRedCubes = 12;
+        var maxGreenCubes = 13;
+        var maxBlueCubes = 14;
 
-        public override string Name => "Cube Conundrum";
+        var possibleGames = games.Where(game =>
+            game.Sets.All(set =>
+                set.RedCubes <= maxRedCubes &&
+                set.GreenCubes <= maxGreenCubes &&
+                set.BlueCubes <= maxBlueCubes));
 
-        private record Set(int RedCubes, int GreenCubes, int BlueCubes);
+        return possibleGames.Select(g => g.Id).Sum();
+    }
 
-        private record Game(int Id, Set[] Sets);
+    protected override int SolvePart2(string input)
+    {
+        var games = ParseGames(input);
 
-        protected override int SolvePart1(string input)
+        var powers = games.Select(game =>
         {
-            var games = ParseGames(input);
+            var maxRed = game.Sets.Max(s => s.RedCubes);
+            var maxGreen = game.Sets.Max(s => s.GreenCubes);
+            var maxBlue = game.Sets.Max(s => s.BlueCubes);
+            
+            return maxRed * maxGreen * maxBlue;
+        });
 
-            var maxRedCubes = 12;
-            var maxGreenCubes = 13;
-            var maxBlueCubes = 14;
+        return powers.Sum();
+    }
 
-            var possibleGames = games.Where(game =>
-                game.Sets.All(set =>
-                    set.RedCubes <= maxRedCubes &&
-                    set.GreenCubes <= maxGreenCubes &&
-                    set.BlueCubes <= maxBlueCubes));
+    private Game[] ParseGames(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+        var games = lines.Select(ParseGame);
 
-            return possibleGames.Select(g => g.Id).Sum();
-        }
+        return games.ToArray();
+    }
 
-        protected override int SolvePart2(string input)
-        {
-            var games = ParseGames(input);
+    private Game ParseGame(string input)
+    {
+        var idMatch = Regex.Match(input, @"Game (?<id>\d*):");
+        var id = int.Parse(idMatch.Groups["id"].Value);
 
-            var powers = games.Select(game =>
-            {
-                var maxRed = game.Sets.Max(s => s.RedCubes);
-                var maxGreen = game.Sets.Max(s => s.GreenCubes);
-                var maxBlue = game.Sets.Max(s => s.BlueCubes);
-                
-                return maxRed * maxGreen * maxBlue;
-            });
+        var sets = ParseSets(input);
 
-            return powers.Sum();
-        }
+        return new Game(id, sets);
+    }
 
-        private Game[] ParseGames(string input)
-        {
-            var lines = input.Split(Environment.NewLine);
-            var games = lines.Select(ParseGame);
+    private Set[] ParseSets(string input)
+    {
+        var setStrs = input.Split(';');
+        return setStrs.Select(ParseSet).ToArray();
+    }
 
-            return games.ToArray();
-        }
+    private Set ParseSet(string input)
+    {
+        var redMatch = Regex.Match(input, @"(?<number>\d*) red");
+        var greenMatch = Regex.Match(input, @"(?<number>\d*) green");
+        var blueMatch = Regex.Match(input, @"(?<number>\d*) blue");
 
-        private Game ParseGame(string input)
-        {
-            var idMatch = Regex.Match(input, @"Game (?<id>\d*):");
-            var id = int.Parse(idMatch.Groups["id"].Value);
+        var red = redMatch.Groups.TryGetValue("number", out var redValue) ?
+            int.Parse(redValue.Value) :
+            0;
 
-            var sets = ParseSets(input);
+        var green = greenMatch.Groups.TryGetValue("number", out var greenValue) ?
+            int.Parse(greenValue.Value) :
+            0;
 
-            return new Game(id, sets);
-        }
+        var blue = blueMatch.Groups.TryGetValue("number", out var blueValue) ?
+            int.Parse(blueValue.Value) :
+            0;
 
-        private Set[] ParseSets(string input)
-        {
-            var setStrs = input.Split(';');
-            return setStrs.Select(ParseSet).ToArray();
-        }
-
-        private Set ParseSet(string input)
-        {
-            var redMatch = Regex.Match(input, @"(?<number>\d*) red");
-            var greenMatch = Regex.Match(input, @"(?<number>\d*) green");
-            var blueMatch = Regex.Match(input, @"(?<number>\d*) blue");
-
-            var red = redMatch.Groups.TryGetValue("number", out var redValue) ?
-                int.Parse(redValue.Value) :
-                0;
-
-            var green = greenMatch.Groups.TryGetValue("number", out var greenValue) ?
-                int.Parse(greenValue.Value) :
-                0;
-
-            var blue = blueMatch.Groups.TryGetValue("number", out var blueValue) ?
-                int.Parse(blueValue.Value) :
-                0;
-
-            return new Set(red, green, blue);
-        }
+        return new Set(red, green, blue);
     }
 }
